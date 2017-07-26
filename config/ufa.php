a1edf0897944c4998fde2025660a631f83e3681f
zhouxiaojian <zhouxiaojian@innotechx.com>
2017-07-26 14:09:44 +0800

    添加lavaral框架

---
+++ config/ufa.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'compatible_ie' => true,
+    'debug' => env('UFA_DEBUG', true),
+    'host' => env('UFA_PLATFORM_HOST', '/'),
+    'debugdir' => ''
+];
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        @include('resources.styles')
+    </head>
+    <body>
+        <!-- main content -->
+        1234test
+    </body>
+</html>
